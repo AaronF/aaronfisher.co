9395387c38131b67bee2ca9c65eac95c3543c67b
Aaron Fisher <aaronfisher.mt@gmail.com>
2014-01-18 13:03:08 +0000

    Added sections

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -12,6 +13,7 @@
     <link rel="stylesheet" href="include/css/style.min.css">
 </head>
 <body>
+<div class="thin_header"></div>
 <div class="header">
     <div class="grid w640">
         <div class="row">
@@ -20,7 +22,9 @@
             </div>
             <h1>Hi, I'm Aaron</h1>
             <h3><a href="http://twitter.com/thetechbox">@TheTechBox</a></h3>
-            <h2>I'm a podcaster and I make stuff on the internet.</h2>
+            <h2 class="rotate_txt">I'm a 
+            	<span class="txt-rotate" data-period="2000" data-rotate='[ "web designer", "web developer", "podcaster", "maker" ]'></span>
+            </h2>
             <div class="cf"></div>
         </div>
     </div>
@@ -32,18 +36,62 @@
 			<p>I am a part web designer and part web developer from the UK. I have worked on a number of projects, some for myself and some for others all of which are detailed below. I also co-host a weekly tech news podcast called 'The Two Techies' of which we have done over 200 episodes in four years!</p>
 			<p>You can find me on <a href="http://twitter.com/thetechbox">Twitter</a>, <a href="http://facebook.com/thetechbox">Facebook</a>, <a href="http://plus.google.com/+AaronFisher">Google+</a> (yes, you read that right) or you can drop me an <a href="mailto:aaron@thetechbox.com">email</a>. I have Tumblr that you can find <a href="http://thetechbox.tumblr.com">here</a>.</p>
 			<p>You can find my GitHub profile at <a href="http://github.com/thetechbox">GitHub.com/TheTechBox</a>.</p>
-			<h2 class="title">Some Projects</h2>
-			<h3><a href="http://farelert.com">Farelert.com</a></h2>
-			<p>Farelert makes it super easy to track the price of a flight over time. All you have to do is enter a few details about your trip and farelert will show you the results, from there you can track multiple flights and get alerted by email or text as soon as it changes. This is currently an ongoing project that we hope to launch in the new year!</p>
-			<div class="hr"></div>
-			<h3><a href="http://getwaiter.com">getWaiter.com</a> (App)</h2>
-			<p>I helped build the iOS & Android app for getWaiter! which is a service that aims to improve your resturant experience. No more waving your hands in the air, simply scan the code on the table and use the app to call the waiter, request more drinks or get the bill.</p>
+			
+		</div>
+	</div>
+</div>
+<div class="sections">
+	<div class="section" id="section1">
+		<div class="grid w900">
+			<div class="image c5">
+				<img src="include/images/farelert.png" >
+			</div>
+			<div class="c1"></div>
+			<div class="text c6">
+				<img src="include/images/farelert_logo_white.png" width="180px">
+				<p>
+					Farelert makes it super easy to track the price of a flight over time. All you have to do is enter a few details about your trip and farelert will show you the results, from there you can track multiple flights and get alerted by email or text as soon as it changes.
+				</p>
+				<a href="http://farelert.com" class="visitbutton float--right">Farelert</a>
+			</div>
+			<div class="cf"></div>
+		</div>
+	</div>
+</div>
+<div class="grid w640 about">
+	<div class="row clear">
+		<div class="c12">
 			<div class="hr"></div>
 			<h3><a href="http://munchtech.tv">MunchTech.tv</a></h2>
 			<p>MunchTech.tv is the home of the podcast that I co-host called The Two Techies It is a weekly technology news show of which we have done over 200 episodes to date. I also designed the WordPress theme that the site uses.</p>
+		</div>
+	</div>
+</div>
+<div class="sections">
+	<div class="section" id="section2">
+		<div class="grid w900">
+			<div class="image c5">
+				<img src="include/images/getwaiter.png" width="280px" class="float--right">
+			</div>
+			<div class="c1"></div>
+			<div class="text c6">
+				<img src="include/images/getwaiter_logo.png" width="180px">
+				<p>
+					I helped build the iOS & Android app for getWaiter! which is a service that aims to improve your resturant experience. No more waving your hands in the air, simply scan the code and use the app to call the waiter, request more drinks or get the bill.
+				</p>
+				<a href="http://getwaiter.com" class="visitbutton float--right">getWaiter!</a>
+			</div>
+			<div class="cf"></div>
+		</div>
+	</div>
+</div>
+<div class="grid w640 about">
+	<div class="row clear">
+		<div class="c12">
 			<div class="hr"></div>
 			<h3><a href="http://syndifeed.com">SyndiFeed.com</a></h2>
 			<p>SyndiFeed was one of my first 'proper' web based projects. SyndiFeed is a basic RSS reader with a built in read later service which has over 2000 users and over 100,000 feeds.</p>
+			<div class="hr"></div>
 		</div>
 	</div>
 </div>
@@ -67,5 +115,7 @@
   ga('send', 'pageview');
 
 </script>
+<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="include/js/home.js"></script>
 </body>
 </html>
