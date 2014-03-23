b654d7d86af935ac516326e61c5d321c4ebaf82f
Aaron Fisher <aaronfisher.mt@gmail.com>
2014-03-23 23:25:18 +0000

    Tweaks to meet markup validation

--- include/js/home.js
+++ include/js/home.js
@@ -1,67 +1,67 @@
 var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
-
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
-
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-  var that = this;
-  var delta = 180 - Math.random() * 100;
-
-  if (this.isDeleting) { delta /= 2; }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
     this.isDeleting = false;
-    this.loopNum++;
-    delta = 180;
-  }
+  };
 
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
 
-window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
-  }
-};
 
-$(document).ready(function(){
-    dug({
-        endpoint: 'https://api.instagram.com/v1/users/1158594/media/recent/?access_token=1158594.5276196.0e70b8dbde224ec7ac799775c2929397&count=8',
-        template: '<ul class="photos">\
-        {{#data}}\
-          <li>\
-            <a href="{{link}}">\
-              <img src="{{images.low_resolution.url}}">\
-            </a>\
-          </li>\
-        {{/data}}\
-        ',
-        target: 'instagram_pictures'
-    });
-});
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 180 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 180;
+    }
+
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+  };
+
+  $(document).ready(function(){
+      dug({
+          endpoint: 'https://api.instagram.com/v1/users/1158594/media/recent/?access_token=1158594.5276196.0e70b8dbde224ec7ac799775c2929397&count=8',
+          template: '<ul class="photos">\
+          {{#data}}\
+            <li>\
+              <a href="{{link}}">\
+                <img src="{{images.low_resolution.url}}">\
+              </a>\
+            </li>\
+          {{/data}}\
+          ',
+          target: 'instagram_pictures'
+      });
+  });
